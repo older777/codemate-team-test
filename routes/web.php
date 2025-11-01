1a364d86cdbec16e52e76f3102701b092d57e41e
older777 <kkkart@mail.ru>
2025-11-01 15:25:34 +0300

    gitignore

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['success' => 'ok'], Response::HTTP_OK);
 });
